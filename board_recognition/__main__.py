60ca1c5ffd25196e9e27b58f7ac264978ec01bda
Paul Chambaz <paul@chambaz.xyz>
2023-02-26 22:31:06 +0100

    added argument parsing

--- board_recognition/__main__.py
+++ board_recognition/__main__.py
@@ -1,11 +1,32 @@
 import board_recognition as br
 
+import sys
+import argparse
+import pathlib
 import matplotlib.image as mimg
 import numpy as np
 
 def main():
 
-    image = mimg.imread('data/0.jpg')
+    parser = argparse.ArgumentParser(description="Get text from a board")
+    parser.add_argument("-i", "--image", type=str, help="Path to the image")
+    args = parser.parse_args()
+
+    if not args.image:
+        parser.print_usage()
+        sys.exit(1)
+
+    image_file = pathlib.Path(args.image)
+    if not image_file.is_file():
+        parser.print_usage()
+        sys.exit(1)
+
+    if image_file.suffix.lower() not in [".jpg", ".jpeg", ".png"]:
+        parser.print_usage()
+        sys.exit(1)
+
+
+    image = mimg.imread(image_file)
 
     gray = br.grayscale(image)
 
